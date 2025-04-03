5269b43422668f906653d362fb5aa764e720d6fc
Arman <contact.armankar@gmail.com>
2025-04-03 15:28:47 +0530

    sub-optimal code

--- server/server.ts
+++ server/server.ts
@@ -1,1 +1,25 @@
-console.log("Hello via Bun!");
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+import connectDatabase from "./src/config/database.ts";
+
+dotenv.config();
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+const PORT = process.env.PORT;
+const startServer = async () => {
+    try {
+        await connectDatabase();
+        app.listen(PORT, () => {
+            console.log(`Server is running on PORT: ${PORT}`);
+        }) ;
+    } catch (error) {
+        console.error("Failed to start server");
+    }
+};
+
+startServer();
